1d347677a6b15095b5e31c494666ee1a82147482
scottypipz <scottypipz@gmail.com>
2021-04-04 16:00:08 +0800

    Added aos@next

--- src/js/aos.js
+++ src/js/aos.js
@@ -15,11 +15,11 @@ AOS.init({
   
 
   // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-  offset: 300, // offset (in px) from the original trigger point
+  offset: 120, // offset (in px) from the original trigger point
   delay: 0, // values from 0 to 3000, with step 50ms
-  duration: 400, // values from 0 to 3000, with step 50ms
+  duration: 500, // values from 0 to 3000, with step 50ms
   easing: 'ease', // default easing for AOS animations
-  once: false, // whether animation should happen only once - while scrolling down
+  once: true, // whether animation should happen only once - while scrolling down
   mirror: false, // whether elements should animate out while scrolling past them
   anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
 
