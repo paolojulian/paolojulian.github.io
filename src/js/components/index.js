0fd314dff200e6c3df01d9fb5138f03aa87b3ac7
scottypipz <scottypipz@gmail.com>
2021-05-10 23:04:30 +0800

    Update colors

--- src/js/components/index.js
+++ src/js/components/index.js
@@ -1,3 +1,5 @@
 import AppButton from './app-button';
+import AppPill from './app-pill';
 
 customElements.define('app-button', AppButton, { extends: 'button' });
+customElements.define('app-pill', AppPill);
