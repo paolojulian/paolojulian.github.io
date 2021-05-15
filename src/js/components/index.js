d3c32077d76f269c337f4fb1325fa2b017619e0e
scottypipz <scottypipz@gmail.com>
2021-05-15 21:30:30 +0800

    Update tech-stack

--- src/js/components/index.js
+++ src/js/components/index.js
@@ -1,9 +1,11 @@
 import AppButton from './app-button';
-import CardWithFrame from './card-w-frame';
+import AppCard from './app-card';
 import AppPill from './app-pill';
+import CardWithFrame from './card-w-frame';
 import ProgressBar from './progress-bar';
 
 customElements.define('app-button', AppButton, { extends: 'button' });
+customElements.define('app-card', AppCard);
 customElements.define('app-pill', AppPill);
-customElements.define('progress-bar', ProgressBar);
 customElements.define('card-w-frame', CardWithFrame);
+customElements.define('progress-bar', ProgressBar);
