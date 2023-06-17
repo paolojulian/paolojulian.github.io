3dcf604b9aaed8c12032eead2c7999edf6bb3038
Paolo Vincent Julian <scottypipz@gmail.com>
2023-06-18 00:45:59 +0800

    fix: Email endpoint

--- src/features/portfolio/hooks/useContactForm.ts
+++ src/features/portfolio/hooks/useContactForm.ts
@@ -1,5 +1,5 @@
 import { useMutation } from "@tanstack/react-query";
-import apiClient from "lib/axios";
+import { emailService } from "lib/axios";
 import { IContactForm } from "../components/Contact/ContactForm";
 import { useState } from "react";
 
@@ -7,7 +7,7 @@ const useContactForm = () => {
   const [isSuccess, setIsSuccess] = useState(false);
   const { mutateAsync: handleSubmit, isLoading } = useMutation({
     mutationFn: async (form: IContactForm) => {
-      const response = await apiClient.post('/api/email/contact-me', form);
+      const response = await emailService.post('/contact-me', form);
       if (!response.ok) {
         throw response;
       }
