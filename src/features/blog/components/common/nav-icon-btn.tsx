4e9074038ce374fd1fcba39a5515fe4dbd55a509
Paolo Vincent Julian <paolovincentarch@gmail.com>
2023-07-21 14:46:42 +0800

    fix: Button to anchor

--- src/features/blog/components/common/nav-icon-btn.tsx
+++ src/features/blog/components/common/nav-icon-btn.tsx
@@ -3,7 +3,7 @@ import React, { FunctionComponent } from 'react';
 
 export type NavIconButtonProps = {
   name: string;
-} & React.HtmlHTMLAttributes<HTMLButtonElement>;
+} & React.HtmlHTMLAttributes<HTMLAnchorElement>;
 
 const NavIconButton: FunctionComponent<NavIconButtonProps> = ({
   children,
@@ -12,22 +12,22 @@ const NavIconButton: FunctionComponent<NavIconButtonProps> = ({
 }) => {
   return (
     <div className='relative'>
-      <button
+      <a
         {...props}
-        className='p-2 rounded-full transition-colors group md:hover:bg-gray-200 active:bg-gray-300 active:scale-[0.97]'
+        className='p-2 rounded-full transition-colors group md:hover:bg-gray-200 active:bg-gray-300 active:scale-[0.97] cursor-pointer flex items-center justify-center'
         aria-haspopup='true'
       >
         {children}
         <div
           className={classNames(
-            'absolute ml-1 left-full -translate-y-1/2 top-1/2 px-4 py-2 bg-gray-800/95 text-gray-50 rounded-lg',
-            'opacity-0 left-0 group-hover:opacity-100 group-hover:left-full',
+            'absolute ml-1 left-full transition-opacity -translate-y-1/2 top-1/2 px-4 py-2 bg-gray-800/95 text-gray-50 rounded-lg',
+            'opacity-0 group-hover:opacity-100',
             'pointer-events-none'
           )}
         >
           {name}
         </div>
-      </button>
+      </a>
     </div>
   );
 };
